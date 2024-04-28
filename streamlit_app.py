95a0d7f91f8e3342970cb506f5bf61ce63ba55e5
SIDDU CHENNAMSETTY <112552964+SiddualieaGit@users.noreply.github.com>
2024-04-28 23:37:41 +0530

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.metrics import accuracy_score
 
